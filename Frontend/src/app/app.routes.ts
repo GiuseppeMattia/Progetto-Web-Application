8cebb72dd2426daf3b998147ed743357535e73dd
MarianoVIsabella <marianovisabella@libero.it>
2025-01-26 17:39:34 +0100

    Inizializzazione componente categorie e homepage

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {CategorieComponent} from './categorie/categorie.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'categories', component:CategorieComponent}
+];
