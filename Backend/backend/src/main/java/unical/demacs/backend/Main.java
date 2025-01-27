5ba85e74df8d49c29e3d20267d2560c34b8d48e3
brunodome <domenico040204@gmail.com>
2025-01-27 09:26:41 +0100

    Implementata e testata classe UtenteDAOJDBC

--- Backend/backend/src/main/java/unical/demacs/backend/Main.java
+++ Backend/backend/src/main/java/unical/demacs/backend/Main.java
@@ -2,9 +2,12 @@ package unical.demacs.backend;
 
 import unical.demacs.backend.model.Annuncio;
 import unical.demacs.backend.model.Recensione;
+import unical.demacs.backend.model.Utente;
 import unical.demacs.backend.persistence.DAO.JDBC.AnnuncioDAOJDBC;
 
+import unical.demacs.backend.persistence.DAO.JDBC.UtenteDAOJDBC;
 import unical.demacs.backend.persistence.DAO.Proxy.AnnuncioProxy;
+import unical.demacs.backend.persistence.DAO.interfaces.UtenteDAO;
 import unical.demacs.backend.persistence.DBManager;
 
 import java.util.List;
@@ -12,29 +15,45 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        AnnuncioDAOJDBC annuncioDAOJDBC = new AnnuncioDAOJDBC(DBManager.getInstance().getConnection());
-        List<Annuncio> annunci = annuncioDAOJDBC.findAll();
+        //TEST Utente DAO - save
 
-        for (Annuncio annuncio : annunci) {
+        /*
+        Utente utente = new Utente();
+        utente.setUsername("Domenico");
+        utente.setPassword("password");
+        utente.setEmail("domenico@gmail.com");
+        utente.setTipo(true);
+        utente.setAmministratore(false);
 
-            Annuncio annuncioProxy = new AnnuncioProxy();
-            annuncioProxy.setID(annuncio.getID());
-            annuncioProxy.setFoto(annuncio.getFoto());
-            annuncioProxy.setCategoria(annuncio.getCategoria());
-            annuncioProxy.setMarca(annuncio.getMarca());
-            annuncioProxy.setModello(annuncio.getModello());
-            annuncioProxy.setPrezzo(annuncio.getPrezzo());
-            annuncioProxy.setDescrizione(annuncio.getDescrizione());
-            annuncioProxy.setTitolo(annuncio.getTitolo());
-            annuncioProxy.setPrezzoScontato(annuncio.getPrezzoScontato());
-            annuncioProxy.setVenditore(annuncio.getVenditore());
+        UtenteDAO utenteDAO = new UtenteDAOJDBC(DBManager.getInstance().getConnection());
+        utenteDAO.save(utente);
+         */
 
-            List<Recensione> recensioni = annuncioProxy.getRecensioni();
+        //TEST Utente DAO - delete
 
-            for (Recensione recensione : recensioni) {
-                System.out.println("Scritta da: " + recensione.getAutore().getUsername());
-                System.out.println("E dice: " + recensione.getTesto());
-            }
-        }
+        /*
+        Utente utente = new Utente();
+        utente.setUsername("Domenico");
+        utente.setPassword("password");
+        utente.setEmail("domenico@gmail.com");
+        utente.setTipo(true);
+        utente.setAmministratore(false);
+
+        UtenteDAO utenteDAO = new UtenteDAOJDBC(DBManager.getInstance().getConnection());
+        utenteDAO.delete(utente);
+         */
+
+        //TEST Utente DAO - findByUsername
+
+        /*
+        UtenteDAO utenteDAO = new UtenteDAOJDBC(DBManager.getInstance().getConnection());
+        Utente utente = utenteDAO.findByUsername("admin");
+        System.out.println(utente.getUsername() + " " + utente.getPassword());
+         */
+
+
+
+
+
     }
 }
--- Backend/backend/src/main/java/unical/demacs/backend/persistence/DAO/JDBC/UtenteDAOJDBC.java
+++ Backend/backend/src/main/java/unical/demacs/backend/persistence/DAO/JDBC/UtenteDAOJDBC.java
@@ -5,6 +5,7 @@ import unical.demacs.backend.persistence.DAO.interfaces.UtenteDAO;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UtenteDAOJDBC implements UtenteDAO {
 
@@ -14,6 +15,8 @@ public class UtenteDAOJDBC implements UtenteDAO {
         this.connection = connection;
     }
 
+
+    //TESTATA E FUNZIONA
     @Override
     public Utente findByUsername(String username) {
 
@@ -39,13 +42,44 @@ public class UtenteDAOJDBC implements UtenteDAO {
         return null;
     }
 
+
+    //TESTATA E FUNZIONA
     @Override
     public void save(Utente utente) {
 
+        String query = "INSERT INTO utente (tipo, username, password, email, amministratore) VALUES (?, ?, ?, ?, ?)";
+
+        try{
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setBoolean(1, utente.getTipo());
+            statement.setString(2, utente.getUsername());
+            statement.setString(3, utente.getPassword());
+            statement.setString(4, utente.getEmail());
+            statement.setBoolean(5, utente.getAmministratore());
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
+
+    //TESTATA E FUNZIONA
     @Override
     public void delete(Utente utente) {
+
+        //PER ELIMINARE, UTILIZZO SOLAMENTE username DATO CHE E' CHIAVE PRIMARIA
+        String query = "DELETE FROM utente WHERE username = ?";
+
+        try{
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, utente.getUsername());
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
