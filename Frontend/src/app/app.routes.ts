e00d5170edeadef4f9f70e01a8be1f3d0af65888
MarianoVIsabella <marianovisabella@libero.it>
2025-01-27 12:20:21 +0100

    Componente creazione aste

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import {CategorieComponent} from './categorie/categorie.component';
 import {HomepageComponent} from './homepage/homepage.component';
 import {ListaUtentiComponent} from './lista-utenti/lista-utenti.component';
+import {CreaComponent} from './crea/crea.component';
 
 export const routes: Routes = [
   {path:'', pathMatch:'full', redirectTo:'/home'},
   {path:'categories', component:CategorieComponent},
   {path:'home', component:HomepageComponent},
-  {path:'users',component:ListaUtentiComponent}
+  {path:'users',component:ListaUtentiComponent},
+  {path:'create',component: CreaComponent}
 ];
