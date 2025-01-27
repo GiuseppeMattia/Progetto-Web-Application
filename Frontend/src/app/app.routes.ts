dbf6b999927ba8fd5200a9b3969fbdb725173bfa
MarianoVIsabella <marianovisabella@libero.it>
2025-01-27 09:55:05 +0100

    Componente categorie html finito + ordine

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import {CategorieComponent} from './categorie/categorie.component';
+import {HomepageComponent} from './homepage/homepage.component';
 
 export const routes: Routes = [
-  {path:'categories', component:CategorieComponent}
+  {path:'', pathMatch:'full', redirectTo:'/home'},
+  {path:'categories', component:CategorieComponent},
+  {path:'home', component:HomepageComponent}
 ];
---
+++ Frontend/src/app/services/api.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  //in questa classe ci saranno le chiamate al backend
+  constructor() { }
+}
