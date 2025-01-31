f2e60f24c4092e2249eabbb329d42a78bf0a5354
MarianoVIsabella <marianovisabella@libero.it>
2025-01-31 16:53:44 +0100

    Creato il componente annuncio

---
+++ Frontend/src/app/annuncio/annuncio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-annuncio',
+  standalone: true,
+  imports: [],
+  templateUrl: './annuncio.component.html',
+  styleUrl: './annuncio.component.css'
+})
+export class AnnuncioComponent {
+
+}
