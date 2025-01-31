e0b15957b9c3a3490ed9c5b1e0a73dc513ec2675
MarianoVIsabella <marianovisabella@libero.it>
2025-01-31 16:15:00 +0100

    Visualizzabili le categorie se registrato

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import  { Routes } from "@angular/router"
 import { LoginComponent } from "./login/login.component"
 import { RegisterComponent } from "./register/register.component"
 import { HomeComponent } from "./home/home.component"
+import {CategorieComponent} from './categorie/categorie.component';
 
 export const routes: Routes = [
   { path: "register", component: RegisterComponent },
   { path: "login", component: LoginComponent },
   { path: "home", component: HomeComponent },
   { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "categories", component: CategorieComponent}
 ]
 
