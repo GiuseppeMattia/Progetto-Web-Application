e8e9b91f5a9a05443879616c86cb82701489e0c5
GiuseppeMattia <gmmezzotero@gmail.com>
2025-02-03 20:40:41 +0100

    Aggiunto il numero di telefono nel form di registrazione

--- Frontend/src/app/register/register.component.ts
+++ Frontend/src/app/register/register.component.ts
@@ -1,10 +1,11 @@
-import {AfterViewChecked, AfterViewInit, Component} from "@angular/core"
+import {AfterViewChecked, AfterViewInit, Component, ElementRef, OnInit, ViewChild} from "@angular/core"
 import { FormBuilder,  FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms"
 import {Router, RouterLink, RouterOutlet, Routes} from "@angular/router"
 import { HttpClientModule } from "@angular/common/http"
 
 import  { UtenteService } from "../services/utente.service"
 import {AuthService} from '../services/auth.service';
+import {UserModel} from '../modelli/userModel';
 
 @Component({
   selector: 'app-register',
@@ -22,9 +23,9 @@ import {AuthService} from '../services/auth.service';
 
 })
 
-export class RegisterComponent{
+export class RegisterComponent implements OnInit{
   registerForm: FormGroup
-
+  @ViewChild('captchaButton', { static: false }) captchaButton!: ElementRef;
   constructor(
     private fb: FormBuilder,
     private router: Router,
@@ -37,17 +38,87 @@ export class RegisterComponent{
       password: ["", [Validators.required, Validators.minLength(6)]],
       tipo: [false, Validators.required],
       amministratore:false,
-      telefono: '',
+      prefisso: ['+39', Validators.required],
+      telefono: ['', [Validators.required, Validators.pattern('^[0-9]{6,15}$')]],
       bannato:false
     })
   }
 
+  ngOnInit() {
+    const script = document.createElement('script');
+    script.src = 'https://www.google.com/recaptcha/api.js';
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+    (window as any).onSubmitCaptcha = (token: string) => this.onSubmitCaptcha(token);
+
+    script.onload = () => {
+      // console.log("reCAPTCHA caricato con successo.");
+      this.inizializzaReCaptcha();
+    };
+  }
+
+  inizializzaReCaptcha() {
+    if ((window as any).grecaptcha) {
+      try {
+        (window as any).grecaptcha.ready(() => {
+          // console.log("reCAPTCHA pronto per l'uso.");
+          (window as any).grecaptcha.reset();
+        });
+      } catch (error) {
+        console.error("Errore nell'inizializzare reCAPTCHA:", error);
+      }
+    } else {
+      // console.error("grecaptcha non è disponibile.");
+    }
+  }
+
+  onSubmit(event: Event) {
+    event.preventDefault(); // Evita il submit immediato
+
+    if (this.registerForm.valid) {
+      (window as any).grecaptcha.render(); // Attiva il reCAPTCHA invisibile
+    } else {
+      alert("Per favore, compila tutti i campi richiesti.")
+    }
+  }
+
+  onSubmitCaptcha(token: string) {
+    // console.log("Token reCAPTCHA ricevuto:", token);
+    this.utenteService.mandaCaptcha(token).subscribe(response => {
+      if (response.success) {
+        //console.log("reCAPTCHA verificato con successo");
+        this.registerUser(); // Se il reCAPTCHA è valido, procedi con il login
+      } else {
+        // console.error("Verifica reCAPTCHA fallita");
+        alert("Verifica reCAPTCHA non riuscita");
+      }
+    }, error => {
+      // console.error("Errore nella verifica reCAPTCHA:", error);
+      alert("Errore nel contattare il server");
+    });
+  }
+
+  fullPhoneNumber(): string {
+    const prefix = this.registerForm.get('prefisso')?.value;
+    const phone = this.registerForm.get('telefono')?.value;
+    return `${prefix}${phone}`;
+  }
+
   registerUser() {
     if (this.registerForm.valid) {
-      const newUser = this.registerForm.value
+      const username = this.registerForm.get("username")?.value || '';
+      const password = this.registerForm.get("password")?.value || '';
+      const tipo = this.registerForm.get("tipo")?.value || false;
+      const email = this.registerForm.get("email")?.value || '';
+      const amministratore = this.registerForm.get("amministratore")?.value || false;
+      const bannato = this.registerForm.get("bannato")?.value || false;
+      const numero = this.fullPhoneNumber();
+
+      const newUser = new UserModel(username, password, tipo, email, amministratore, bannato, numero);
       this.utenteService.creaUtente(newUser).subscribe(
         (response) => {
-          console.log("Utente registrato con successo", response)
+          // console.log("Utente registrato con successo", response)
           this.router.navigate(["/home"])
         },
         (error) => {
@@ -63,6 +134,8 @@ export class RegisterComponent{
     } else {
       alert("Per favore, compila tutti i campi richiesti correttamente.")
     }
+    this.inizializzaReCaptcha()
   }
+
 }
 
