8d7e00125a20dd9915ea4c4c2ac5bf362f37d582
GiuseppeMattia <gmmezzotero@gmail.com>
2025-02-05 12:46:57 +0100

    Aggiunta la pagina del profilo dei venditori quando si clicca sull'username negli annunci

---
+++ Frontend/src/app/venditore/venditore.component.ts
@@ -0,0 +1,69 @@
+import {Component, OnInit} from '@angular/core';
+import {Annuncio} from '../modelli/annuncio.model';
+import {AnnuncioService} from '../services/annuncio.service';
+import {AuthService} from '../services/auth.service';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
+import {ActivatedRoute, Router, RouterLink} from '@angular/router';
+import {NgForOf, NgIf} from '@angular/common';
+
+@Component({
+  selector: 'app-venditore',
+  standalone: true,
+  imports: [
+    NgForOf,
+    NgIf,
+    RouterLink
+  ],
+  templateUrl: './venditore.component.html',
+  styleUrl: './venditore.component.css'
+})
+export class VenditoreComponent implements OnInit {
+  annunci: Annuncio[] = [];
+  errorMessage = '';
+  username: string|null = null;
+
+  constructor(
+    private annuncioService: AnnuncioService,
+    private authService: AuthService,
+    private sanitizer: DomSanitizer,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.username = params.get('username'); // Prende il parametro dalla route
+      // console.log('ID dalla route:', this.id);
+    });
+    this.loadAnnunci();
+  }
+
+  loadAnnunci() {
+    if (this.username) {
+      this.annuncioService.trovaByUtente(this.username).subscribe(
+        (annunci) => {
+          this.annunci = annunci;
+        },
+        (error) => {
+          this.errorMessage = 'Errore nel caricamento degli annunci.';
+        }
+      );
+    } else {
+      this.errorMessage = 'Utente non autenticato.';
+    }
+  }
+
+  getImageUrl(byte: string): SafeUrl {
+    const byteCharacters = atob(byte);
+    const byteArray = new Uint8Array(byteCharacters.length);
+
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteArray[i] = byteCharacters.charCodeAt(i);
+    }
+
+    const blob = new Blob([byteArray], { type: 'image/png' });
+    const url = URL.createObjectURL(blob);
+    return this.sanitizer.bypassSecurityTrustUrl(url);
+  }
+
+
+}
